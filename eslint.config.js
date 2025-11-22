c5a08b3d9f4666005ca32bbb6c91fef9e474e8c2
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-11-22 14:29:00 -0800

    Upgrades

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,13 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 import prettierConfig from 'eslint-config-prettier'
 
-export default [...tanstackConfig, prettierConfig]
+export default [
+  ...tanstackConfig,
+  prettierConfig,
+  ...storybook.configs["flat/recommended"]
+];
