6a33719fa718cd2db359174b9afb058a8531cfb8
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-11-30 20:20:05 -0800

    Fix lock color

--- src/routes/(core)/admin/index.tsx
+++ src/routes/(core)/admin/index.tsx
@@ -17,7 +17,7 @@ function AdminPage() {
 		<div className="flex flex-col flex-1 w-full max-w-2xl px-2 animate-page-in">
 			<div className="relative flex flex-col flex-1 gap-4">
 				<h1 className="flex flex-row items-center gap-2 text-red-500">Admin</h1>
-				<Lock className="size-18 text-red-500 opacity-30 absolute left-4 -top-4 -z-10" />
+				<Lock className="size-18 text-red-500/30 absolute left-4 -top-4 -z-10" />
 				{/*  */}
 				<Card>
 					<CardHeader>
@@ -63,9 +63,7 @@ function AdminPage() {
 					</CardHeader>
 					<CardContent>
 						<Suspense fallback={<Skeleton className="h-10 w-full" />}>
-							{/* <ClientOnly> */}
 							<UsersList />
-							{/* </ClientOnly> */}
 						</Suspense>
 					</CardContent>
 				</Card>
