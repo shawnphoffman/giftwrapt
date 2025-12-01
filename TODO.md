b7363eafa149ac02fb7494be50551f142646a9ba
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-11-30 18:46:39 -0800

    Avatar?

--- TODO.md
+++ TODO.md
@@ -4,3 +4,16 @@
 
 [ ] Initialization script with Docker
 [ ] Create the first user as admin
+
+
+## ShadCN Extensions
+
+- https://better-upload.com/?utm_source=ui.shadcn.com&utm_medium=referral&utm_campaign=directory
+- https://www.diceui.com/docs/components/avatar-group
+- https://www.diceui.com/docs/components/badge-overflow
+- https://www.diceui.com/docs/components/kanban
+- https://www.diceui.com/docs/components/scroller
+- https://www.diceui.com/docs/components/stepper
+- https://www.diceui.com/docs/components/tour
+- https://www.diceui.com/docs/utilities/client-only
+-
---
+++ src/components/common/user-avatar.tsx
@@ -0,0 +1,25 @@
+// import { cn } from '@/lib/utils'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+
+type UserAvatarProps = {
+	name: string
+	image?: string | null
+	className?: string
+}
+
+export default function UserAvatar({ name, image, className }: UserAvatarProps) {
+	const initials = name?.charAt(0).toUpperCase() || '?'
+	return (
+		// <Avatar className={cn('w-8 h-8 border border-foreground', className)}>
+		<Avatar className={'w-8 h-8 border border-foreground'}>
+			<AvatarImage
+				src={image || undefined}
+				alt={name || 'User avatar'}
+				onLoadingStatusChange={status => {
+					console.log('damn', status)
+				}}
+			/>
+			<AvatarFallback className="font-bold bg-background text-foreground leading-none">{initials}</AvatarFallback>
+		</Avatar>
+	)
+}
