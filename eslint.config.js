121ac3f97d5755ffc10db0a95f79bb2589f2e078
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-12-04 11:26:25 -0800

    Add eslint import sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
-//  @ts-check
-
+import storybook from 'eslint-plugin-storybook'
 import { tanstackConfig } from '@tanstack/eslint-config'
 import prettierConfig from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default [
-  ...tanstackConfig,
-  prettierConfig,
-  ...storybook.configs["flat/recommended"]
-];
+	...tanstackConfig,
+	prettierConfig,
+	...storybook.configs['flat/recommended'],
+	{
+		plugins: {
+			'simple-import-sort': simpleImportSort,
+		},
+		rules: {
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+		},
+	},
+]
