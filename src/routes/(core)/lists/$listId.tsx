51fe3a88a664a6a5cb3e250fa12f9bb18d41c6ca
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2025-12-13 10:42:05 -0800

    Add user avatar badge

--- src/routes/(core)/lists/$listId.tsx
+++ src/routes/(core)/lists/$listId.tsx
@@ -1,9 +1,12 @@
 import { createFileRoute, notFound, redirect } from '@tanstack/react-router'
 
 import { getListForViewing } from '@/api/lists'
+import ListTypeIcon from '@/components/common/list-type-icon'
 import UserAvatar from '@/components/common/user-avatar'
-import { Badge } from '@/components/ui/badge'
-import { ListTypes } from '@/db/schema/enums'
+import UserAvatarBadge from '@/components/common/user-avatar-badge'
+import LoadingSkeleton from '@/components/skeletons/loading-skeleton'
+// import { Badge } from '@/components/ui/badge'
+// import { ListTypes } from '@/db/schema/enums'
 
 export const Route = createFileRoute('/(core)/lists/$listId')({
 	loader: async ({ params }) => {
@@ -29,21 +32,28 @@ function ListDetailPage() {
 	const list = Route.useLoaderData()
 
 	const recipientName = list.owner.name || list.owner.email
-	const listTypeLabel = ListTypes[list.type]
+	// const listTypeLabel = ListTypes[list.type]
 
 	return (
 		<div className="wish-page">
-			<div className="flex items-center gap-3">
+			<div className="flex flex-col flex-1 gap-6">
 				{/* HEADING */}
-				<div className="flex flex-col gap-0.5 min-w-0">
-					<div className="flex items-center gap-2 min-w-0">
-						<h1 className="truncate">{list.name}</h1>
-						<Badge variant="outline" className="whitespace-nowrap">
-							{listTypeLabel}
-						</Badge>
+				<div className="flex items-center gap-3 relative">
+					<div className="flex flex-col gap-0.5 min-w-0">
+						<div className="flex items-center gap-2 min-w-0">
+							<h1 className="truncate">{list.name}</h1>
+							{/* <Badge variant="outline" className="whitespace-nowrap">
+								{listTypeLabel}
+							</Badge> */}
+						</div>
+						<UserAvatarBadge name={recipientName} image={list.owner.image} />
+						<UserAvatar name={recipientName} image={list.owner.image} size="small" />
+						<div className="text-sm text-muted-foreground truncate">{recipientName}</div>
 					</div>
-					<div className="text-sm text-muted-foreground truncate">{recipientName}</div>
+					<ListTypeIcon type={list.type} className="wish-page-icon" />
 				</div>
+				{/* CONTENT */}
+				<LoadingSkeleton />
 			</div>
 		</div>
 	)
